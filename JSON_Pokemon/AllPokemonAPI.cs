cf34e0866298075180966ff431bb5c1327032d92
jJessica Wren <jessica.wren@ou.edu>
2021-02-26 14:35:19 -0600

    Worked on pokemon

---
+++ JSON_Pokemon/AllPokemonAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Pokemon
+{
+    public class AllPokemonAPI // The first ever curly bracket
+    {
+        // we skipped count next and previous
+
+        public List<ResultObject> results { get; set; }
+
+    }
+
+    public class ResultObject
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
